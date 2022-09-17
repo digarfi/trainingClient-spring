a3256d1237f18ee4cc086ceaa556670424e84e23
digarfi <digarfi@gmail.com>
2022-09-17 19:07:52 -0300

    Project created

---
+++ backend/src/main/java/com/digarfi/trainingClient/TrainingClientApplication.java
@@ -0,0 +1,13 @@
+package com.digarfi.trainingClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingClientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/digarfi/trainingClient/TrainingClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digarfi.trainingClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
